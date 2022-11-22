e7c0677263743e31099d6e2ac4afd85423970415
youin salif <salifyouin@gmail.com>
2022-11-22 14:43:54 +0000

    Fist commit

---
+++ src/main/java/com/exercice/awsjava1/AwsJava1Application.java
@@ -0,0 +1,13 @@
+package com.exercice.awsjava1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJava1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsJava1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercice/awsjava1/AwsJava1ApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.exercice.awsjava1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJava1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
